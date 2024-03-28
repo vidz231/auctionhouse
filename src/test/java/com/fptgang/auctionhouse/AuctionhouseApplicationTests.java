25a01efaac855a651aaac71bd60e2bcb7537cc38
Vi Le <viltse180769@fpt.edu.vn>
2024-03-28 12:49:33 +0700

    project init

---
+++ src/test/java/com/fptgang/auctionhouse/AuctionhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fptgang.auctionhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
