877f1fa5c1bbee72cb405bfb3113ec69187a08fb
Vi Le <viltse180769@fpt.edu.vn>
2024-03-28 14:12:11 +0700

    Update Maven and Spring Boot configurations

--- src/main/java/com/fptgang/auctionhouse/AuctionhouseApplication.java
+++ src/main/java/com/fptgang/auctionhouse/AuctionhouseApplication.java
@@ -2,8 +2,9 @@ package com.fptgang.auctionhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class AuctionhouseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuctionhouseApplication.class, args);
