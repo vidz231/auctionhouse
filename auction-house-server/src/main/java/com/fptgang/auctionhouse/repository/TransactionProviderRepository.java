fe04132d0c19541f5570fcfa52e0c3abf02471c1
Truong <schoolpear14@gmail.com>
2024-04-05 20:44:24 +0700

    dashboard

--- auction-house-server/src/main/java/com/fptgang/auctionhouse/repository/TransactionProviderRepository.java
+++ auction-house-server/src/main/java/com/fptgang/auctionhouse/repository/TransactionProviderRepository.java
@@ -3,4 +3,5 @@ package com.fptgang.auctionhouse.repository;
 import com.fptgang.auctionhouse.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionProvider extends JpaRepository<Transaction, Long> {}
+public interface TransactionProviderRepository
+  extends JpaRepository<Transaction, Long> {}
