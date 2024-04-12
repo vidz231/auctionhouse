524e86e09e3039f63377ec1a73beb93c4d68ee4a
Vi Le <115398831+vidz231@users.noreply.github.com>
2024-04-12 09:37:16 +0700

    add spring validation and projection for pro

---
+++ auction-house-server/src/main/java/com/fptgang/auctionhouse/projection/ProductProjection.java
@@ -0,0 +1,31 @@
+package com.fptgang.auctionhouse.projection;
+
+import com.fptgang.auctionhouse.model.Country;
+import com.fptgang.auctionhouse.model.Orchid;
+import com.fptgang.auctionhouse.model.Product;
+import com.fptgang.auctionhouse.model.User;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.sql.Date;
+
+@Projection(name = "getAllValues",types = Product.class)
+public interface ProductProjection {
+     Long getId() ;
+
+     User getOwner() ;
+
+     String getName() ;
+
+     Double getPrice() ;
+
+     Orchid getOrchid() ;
+
+     String getDescription() ;
+     Country getMadeIn() ;
+
+     Integer getRemain() ;
+
+     Date getCreatedAt() ;
+
+     Date getUpdatedAt();
+}
